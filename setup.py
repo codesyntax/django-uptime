845e1bec0f20e180e920532a6171404d8bf531a0
Urtzi Odriozola <uodriozola@codesyntax.com>
2022-11-22 16:29:37 +0100

    package setup + test ci

--- setup.py
+++ setup.py
@@ -21,7 +21,9 @@ setup(
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[],
+    install_requires=[
+        "Django>=1.11",
+    ],
     entry_points="""
       # -*- Entry points: -*-
       """,
